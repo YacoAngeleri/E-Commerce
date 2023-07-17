c58f5a5517176346a122aaab2ad90d42e6e0c89d
Tomas Yaco Angeleri <yacoang@gmail.com>
2023-07-17 18:10:58 -0300

    validacion formularios hecha

---
+++ JS/validacionFormulario.js
@@ -0,0 +1,76 @@
+
+function valida(input) {
+  const tipoDeInput = input.dataset.tipo;
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
+
+  if (input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML = "";
+  } else {
+    input.parentElement.classList.add("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML =
+      mostrarMensajeDeError(tipoDeInput, input);
+  }
+}
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("blur", (input) => {
+    valida(input.target);
+  });
+});
+
+const tipoDeErrores = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "customError",
+];
+
+const mensajesDeError = {
+  nombre: {
+    valueMissing: "El campo nombre no puede estar vacío",
+    patternMismatch: "El maximo de caracteres para este campo es de 50",
+  },
+  password: {
+    valueMissing: "El campo contraseña no puede estar vacío",
+    patternMismatch:
+      "Al menos 6 caracteres, máximo 12, debe contener un número, una letra minúscula y una letra mayúscula.",
+  },
+  imagen: {
+    valueMissing: "El campo imagen no puede estar vacío",
+    typeMismatch: "La imagen no es válida",
+  },
+  categoria: {
+    valueMissing: "Seleccione una categoria valida: Dexter y Moov, Rukawe o Kimikal",
+    patternMismatch: "Seleccione una categoria valida: Dexter y Moov, Rukawe o Kimikal",
+  },
+  precio: {
+    valueMissing: "El campo precio no puede estar vacío",
+    typeMismatch: "El precio no es válido",
+  },
+  descripcion: {
+    valueMissing: "El campo descripción no puede estar vacío",
+    patternMismatch: "La descripción debe contener entre 2 y 200 caracteres.",
+  },
+};
+
+const validadores = {
+  validar: (input) => validacion(input),
+};
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+  let mensaje = "";
+  tipoDeErrores.forEach((error) => {
+    if (input.validity[error]) {
+      console.log(tipoDeInput, error);
+      console.log(input.validity[error]);
+      console.log(mensajesDeError[tipoDeInput][error]);
+      mensaje = mensajesDeError[tipoDeInput][error];
+    }
+  });
+  return mensaje;
+}
